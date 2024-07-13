c8f9cae22ab6b62057766d0334f60c0fed349f6b
Airton Carneiro <airtoncarneiro@gmail.com>
2024-07-13 16:31:45 -0300

    Refatorando para um novo leiaute

---
+++ assets/custom.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Adicione seu JavaScript aqui
+    console.log('Custom JavaScript carregado!');
+});
--- index.md
+++
@@ -1,3 +0,0 @@
-## Sim! Não está acabado.
-
-[Tutorial para configurar um blog no github](https://chadbaldwin.net/2021/03/14/how-to-build-a-sql-blog.html)
